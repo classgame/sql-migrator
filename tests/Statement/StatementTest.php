b0f02dc2de907dcffe1641d0ddeb0eb385708ed5
João Paulo Ferreira <joao.paulo@zage.com.br>
2020-11-04 01:28:32 -0300

    fix test 'testShouldCreateStatement'

--- tests/Statement/StatementTest.php
+++ tests/Statement/StatementTest.php
@@ -7,26 +7,19 @@ use SqlMigrator\Statement\Statement;
 
 class StatementTest extends TestCase
 {
-    public function testShouldCreateStatement()
+    public function testShouldCreateStatement(): void
     {
         $command = 'insert into ...';
         $startLine = 1;
-        $startSpace = 0;
-        $endLine = 4;
-        $endSpace = 20;
+        $startPosition = 1;
 
         $stmt = new Statement(
             $command,
-            $startLine,
-            $endLine,
-            $startSpace,
-            $endSpace
+            $command
         );
 
         $this->assertEquals($command, $stmt->getCommand());
         $this->assertEquals($startLine, $stmt->getStartLine());
-        $this->assertEquals($endLine, $stmt->getEndLine());
-        $this->assertEquals($startSpace, $stmt->getStartSpace());
-        $this->assertEquals($endSpace, $stmt->getEndSpace());
+        $this->assertEquals($startPosition, $stmt->getStartPosition());
     }
 }
